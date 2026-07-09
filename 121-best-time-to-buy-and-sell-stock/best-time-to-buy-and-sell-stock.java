26dfdab34c9e8f1ca69b35c4dfb23548a4795d06
Tarun Gangwar <tarungangwar379@gmail.com>
2026-07-09 20:36:37 +0530

    Time: 2 ms (42.12%) | Memory: 94.5 MB (32.83%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,19 @@
+ // optimize solution 
+ class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 1;i < n; i++){
+            if( prices[i] < minPrice){
+                minPrice = prices[i];
+            }else{
+                int profit = prices[i] - minPrice;
+                maxProfit = Math.max(maxProfit,profit);
+            }
+        }
+        return maxProfit;
+
+        
+    }
+}
